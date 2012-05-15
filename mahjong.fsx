851699aac83c4ff012fbde2b035a042996a0db19
RShestopalyuk <RShestopalyuk@slb.com>
2012-05-15 15:41:06 +0200

    - more consistent variable naming

--- mahjong.fsx
+++ mahjong.fsx
@@ -5,12 +5,21 @@
 #r "PresentationCore"
 #r "PresentationFramework"
 #r "System.Xaml"
-#time 
+
+open System
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Shapes
+open System.Windows.Media
+open System.Windows.Media.Imaging
+open System.Windows.Markup
+open System.Xml
+open System.IO
 
 //============================================================================================
 //  General utils
 //============================================================================================
-//  Shuffles array in-place (good old Fished-Yites)
+//  Shuffles array in-place (good old Fisher-Yates)
 let shuffle arr =
   let a = arr |> Array.copy
   let rand = new System.Random()
@@ -53,17 +62,12 @@ let rec tryApply maxDepth f arg =
 //============================================================================================
 //  UI/Drawing
 //============================================================================================
-open System
-open System.Windows
-open System.Windows.Controls
-open System.Windows.Shapes
-open System.Windows.Media
-open System.Windows.Media.Imaging
-open System.Windows.Markup
-open System.Xml
-open System.IO
-
 let fullPath file = __SOURCE_DIRECTORY__ + "/"+ file
+
+type StoneState =
+  | Visible
+  | Selected
+  | Hidden
 
 let loadXamlWindow (filename:string) =
   let reader = XmlReader.Create(filename)
@@ -83,39 +87,16 @@ type SpriteAtlas = {
 
 let spriteBrush atlas id =
   let imgSource = new BitmapImage(new Uri(fullPath atlas.File))
-  let cardW, cardH = 1./(float atlas.Cols), 1./(float atlas.Rows)
-  let viewBox = new Rect(cardW*(float (id % atlas.Cols)), 
-                         cardH*(float (id / atlas.Cols)), cardW, cardH)
+  let stoneW, stoneH = 1./(float atlas.Cols), 1./(float atlas.Rows)
+  let viewBox = new Rect(stoneW*(float (id % atlas.Cols)), 
+                         stoneH*(float (id / atlas.Cols)), stoneW, stoneH)
   new ImageBrush(ImageSource = imgSource, Viewbox = viewBox)
 
-let bgAtlas = {
-  File = "brick.png";
-  Cols = 2; Rows = 1;
-  FrameWidth = 74.; FrameHeight = 85.;
-}
+let bgAtlas = { File = "stones_bg.png"; Cols = 2; Rows = 1; FrameWidth = 74.; FrameHeight = 85.; }
+let fgAtlas = { File = "stones_fg.png"; Cols = 12; Rows = 9; FrameWidth = 64.; FrameHeight = 60.; }
 
-let fgAtlas = {
-  File = "cards.png";
-  Cols = 12; Rows = 9;
-  FrameWidth = 64.; FrameHeight = 60.;
-}
-
-let createBrick x y id =
-  let bg = new Rectangle(Width=bgAtlas.FrameWidth, Height=bgAtlas.FrameHeight)
-  Canvas.SetLeft(bg, x)
-  Canvas.SetTop(bg, y)   
-  let fg = new Rectangle(Width=fgAtlas.FrameWidth, Height=fgAtlas.FrameHeight)
-  Canvas.SetLeft(fg, x + 2.)
-  Canvas.SetTop(fg, y + 10.)   
-  (bg, fg)
-
-type CardState =
-  | Visible
-  | Selected
-  | Hidden
-
-let updateCardVisual cardControls id state = 
-  let (bg:Rectangle), (fg:Rectangle) = cardControls
+let updateStoneControl stoneControls id state = 
+  let (bg:Rectangle), (fg:Rectangle) = stoneControls
   let spriteID = if state = Selected then 1 else 0
   let selBrush = spriteBrush bgAtlas spriteID
   match state with
@@ -127,40 +108,34 @@ let updateCardVisual cardControls id state =
     | _ -> spriteBrush fgAtlas id
   fg.Fill <- imgBrush
 
-let setCardOpacity opacity (fg:Rectangle, bg:Rectangle) =
+let setStoneOpacity opacity (fg:Rectangle, bg:Rectangle) =
     fg.Opacity <- opacity; bg.Opacity <- opacity;
 
-let cellSz = 64., 75.
-let levShift = -7., -10.
+let STONE_EXTENTS = 64., 75.
+let STONE_3D_OFFSET = -7., -10.
 
-let cellCoord (i, j, level) =
-  let lx, ly = levShift
-  let x = (float i)*(fst cellSz)*0.5 + (float level)*lx
-  let y = (float j)*(snd cellSz)*0.5 + (float level)*ly
-  (x, y, fst cellSz - lx, snd cellSz - ly)
+let getStoneCoords (i, j, level) =
+  let lx, ly = STONE_3D_OFFSET
+  let sx, sy = STONE_EXTENTS
+  let x = (float i)*sx*0.5 + (float level)*lx
+  let y = (float j)*sy*0.5 + (float level)*ly
+  (x, y, sx - lx, sy - ly)
 
-let createCell (id, (i, j, level)) =
-  let (x, y, _, _) = cellCoord(i, j, level)
-  let cardControls = createBrick x y id
-  updateCardVisual cardControls id Visible
-  cardControls
+let createStoneControls (id, (i, j, level)) =
+  let (x, y, _, _) = getStoneCoords(i, j, level)
+  let bg = new Rectangle(Width=bgAtlas.FrameWidth, Height=bgAtlas.FrameHeight)
+  Canvas.SetLeft(bg, x)
+  Canvas.SetTop(bg, y)   
+  let fg = new Rectangle(Width=fgAtlas.FrameWidth, Height=fgAtlas.FrameHeight)
+  Canvas.SetLeft(fg, x + 2.)
+  Canvas.SetTop(fg, y + 10.)
+  let stoneControls = (bg, fg)
+  updateStoneControl stoneControls id Visible
+  stoneControls
 
 //============================================================================================
 //  Layouts  
 //============================================================================================
-let layouts = 
-  let splitSections (res, s) (line:string) = 
-    if line.StartsWith("-") then (s::res, "") else (res, s + "\n" + line)
-  seq {
-      use sr = new StreamReader(fullPath "layouts.txt")
-      while not sr.EndOfStream do yield sr.ReadLine()
-  } |> Seq.fold splitSections ([],"") 
-    |> fst 
-    |> List.rev 
-    |> List.filter (fun l -> l.Length > 0) 
-    |> List.toArray 
-    |> choosei (function | i, x when i%2 = 1 -> Some x | _ -> None)
-
 let parseLayout (str:string) =   
   let charToHeight ch = 
     match Int32.TryParse(string ch) with
@@ -189,6 +164,21 @@ let parseLayout (str:string) =
           shifts |> Array.iter (fun (x, y) -> l.[row + x].[col + y] <- level - 1) 
   } |> Seq.toArray
 
+let layouts = 
+  let splitSections (res, s) (line:string) = 
+    if line.StartsWith("-") then (s::res, "") else (res, s + "\n" + line)
+  seq {
+      use sr = new StreamReader(fullPath "layouts.txt")
+      while not sr.EndOfStream do yield sr.ReadLine()
+  } |> Seq.fold splitSections ([],"") 
+    |> fst 
+    |> List.rev 
+    |> List.filter (fun l -> l.Length > 0) 
+    |> List.toArray 
+    |> choosei (function | i, x when i%2 = 1 -> Some x | _ -> None)
+    |> Array.map parseLayout
+    |> Array.map (Array.sortBy (fun (x, y, h) -> x + y + h*1000))
+
 let langs = 
   seq {
         use sr = new StreamReader(fullPath "languages.txt")
@@ -199,20 +189,20 @@ let langs =
     |> Seq.map (fun el -> (el.[0], el.[1]))
     |> Seq.toArray
 
-let numCardTypes = Array.length langs
+let numStoneTypes = Array.length langs
 //============================================================================================
 //  CARD FUNCTIONS
 //============================================================================================
 let isFree coords = 
   //  create hash table with coordinates to quickly find neighbors
   let m = System.Collections.Generic.Dictionary()
-  let addCell i (x, y, h) = 
+  let addStone i (x, y, h) = 
     [|0, 0; 1, 0; 0, 1; 1, 1|]
     |> Array.iter (fun (dx, dy) -> m.Add((x + dx, y + dy, h), i))
-  coords |> Array.iteri addCell
-  //  return function which checks if given card is free (using the cached hash table)
-  fun (states:CardState[]) cardID ->
-    let (x, y, h) = coords.[cardID]
+  coords |> Array.iteri addStone
+  //  return function which checks if given stone is free (using the cached hash table)
+  fun (states:StoneState[]) stoneID ->
+    let (x, y, h) = coords.[stoneID]
     let isBlockedBy offsets = 
       let isBlocking (dx, dy, dh) =
         let key = (x + dx, y + dy, h + dh)
@@ -223,7 +213,7 @@ let isFree coords =
     let right = [2, 0, 0; 2, 1, 0]
     not (isBlockedBy top || (isBlockedBy left && isBlockedBy right))
 
-let getFree coords (states:CardState[]) =
+let getFree coords (states:StoneState[]) =
   [0 .. states.Length - 1]
   |> List.filter (fun i -> states.[i] <> Hidden)
   |> List.filter (isFree coords states)
@@ -238,19 +228,19 @@ let getMatches (ids:int[]) coords states =
       | _ -> 0) free) > 1)
   |> Seq.toList
 
-let arrangeRandom (coords:(int*int*int)[]) (cardTypes:int[]) = 
-  Some(cardTypes |> shuffle)
+let arrangeRandom (coords:(int*int*int)[]) (stoneTypes:int[]) = 
+  Some(stoneTypes |> shuffle)
 
-let tryArrangeSolvable (coords:(int*int*int)[]) (cardTypes:int[]) = 
-  let cardPairTypes =
-    cardTypes
+let tryArrangeSolvable (coords:(int*int*int)[]) (stoneTypes:int[]) = 
+  let stonePairTypes =
+    stoneTypes
     |> Array.sort
     |> choosei (function | i, x when i%2 = 0 -> Some x | _ -> None)
     |> shuffle
   let isFree' = isFree coords
   let s = seq { 
     let states = [|for x in 1 .. coords.Length do yield Visible|]    
-    for c in cardPairTypes do
+    for c in stonePairTypes do
       let nextFree = 
         [0..(coords.Length - 1)]
         |> List.filter (fun x -> states.[x] = Visible)
@@ -263,12 +253,12 @@ let tryArrangeSolvable (coords:(int*int*int)[]) (cardTypes:int[]) =
       yield! nextFree
   } 
   let ids = s |> Seq.toArray |> Array.sortBy fst |> Array.map snd 
-  let numCards = Seq.length cardTypes
-  if ids.Length <> numCards then None else Some ids
+  let numStones = Seq.length stoneTypes
+  if ids.Length <> numStones then None else Some ids
 
 let MAX_ARRANGE_ATTEMPTS = 50
 
-let shuffleVisible coords (ids:int[]) (states:CardState[]) shuffleFn =
+let shuffleVisible coords (ids:int[]) (states:StoneState[]) shuffleFn =
   let isVisible = (function | i, id when states.[i] = Visible -> Some id | _ -> None)
   let shuffled = 
     ids
@@ -282,11 +272,6 @@ let shuffleVisible coords (ids:int[]) (states:CardState[]) shuffleFn =
     |> Array.iteri (fun i (_, idx, _) -> ids.[idx] <- s.[i])
   | None -> MessageBox.Show "Not possible to create solvable position!" |> ignore
 
-let genCardCoords layoutID =
-  layouts.[layoutID]
-  |> parseLayout 
-  |> Array.sortBy (fun (x, y, h) -> x + y + h*1000)
-
 let getMaxLevel coords = 
   let  _, _, m = coords |> Array.maxBy (fun (_, _, h) -> h)
   m
@@ -295,19 +280,19 @@ let getMaxLevel coords =
 //  GAME DATA STRUCTURE
 //============================================================================================
 type Game = 
-  { cardCoords: (int*int*int)[]
-    cardStates: CardState[]
-    cardIDs: int[]
-    cardControls:(Rectangle*Rectangle)[] 
+  { stoneCoords: (int*int*int)[]
+    stoneStates: StoneState[]
+    stoneIDs: int[]
+    stoneControls:(Rectangle*Rectangle)[] 
     mutable curSelected:int option
     mutable moves:int list
     mutable numHiddenLevels: int }
 
 let newGame layoutID =
-  let coords = genCardCoords layoutID
+  let coords = layouts.[layoutID]
   let states = Array.init coords.Length (fun _ -> Visible)
   let ids = 
-    [|1..numCardTypes|] 
+    [|1..numStoneTypes|] 
     |> shuffle 
     |> Seq.truncate ((Array.length coords)/4)
     |> replicate 4
@@ -316,18 +301,18 @@ let newGame layoutID =
   let controls = 
     coords
     |> Array.zip ids
-    |> Array.map createCell
+    |> Array.map createStoneControls
   canvas.Children.Clear()
   controls 
     |> Array.iter (fun (bg, fg) -> canvas.Children.Add(bg) |> ignore
                                    canvas.Children.Add(fg) |> ignore)
-  { cardCoords =  coords
-    cardStates = states
+  { stoneCoords =  coords
+    stoneStates = states
     curSelected = None
     moves = []
-    cardIDs = ids
+    stoneIDs = ids
     numHiddenLevels = 0
-    cardControls = controls }
+    stoneControls = controls }
 
 //============================================================================================
 //  MUTABLE STATE
@@ -337,81 +322,84 @@ let mutable game = newGame ((new System.Random()).Next(0, Array.length layouts))
 //============================================================================================
 //  GAME CODE
 //============================================================================================
-let updateCardVisuals () =
-  game.cardStates 
-  |> Array.iteri (fun i state -> updateCardVisual game.cardControls.[i] game.cardIDs.[i] state)
+let updateStoneControls () =
+  game.stoneStates 
+  |> Array.iteri (fun i state -> updateStoneControl game.stoneControls.[i] game.stoneIDs.[i] state)
   
-let setCardState state idx =
-  game.cardStates.[idx] <- state
-  updateCardVisual game.cardControls.[idx] game.cardIDs.[idx] state
+let setStoneState state idx =
+  game.stoneStates.[idx] <- state
+  updateStoneControl game.stoneControls.[idx] game.stoneIDs.[idx] state
 
 let undoMove () = 
   match game.moves with
-  | a::b::rest -> game.moves <- rest; setCardState Visible a; setCardState Visible b;
+  | a::b::rest -> game.moves <- rest; setStoneState Visible a; setStoneState Visible b;
   | _ -> ()
 
-let pickCell mx my = 
-    game.cardCoords 
-    |> Array.map cellCoord 
+let pickStone mx my = 
+    game.stoneCoords 
+    |> Array.map getStoneCoords 
     |> tryFindLastIndexi (fun n (x, y, w, h) -> 
-      game.cardStates.[n] <> Hidden && mx > x && my > y && mx < x + w && my < y + h)
+      game.stoneStates.[n] <> Hidden && mx > x && my > y && mx < x + w && my < y + h)
 
 let unselectAll () = 
     game.curSelected <- None
-    [|0 .. game.cardCoords.Length - 1|] 
-    |> Array.filter (fun i -> game.cardStates.[i] <> Hidden) 
-    |> Array.iter (setCardState Visible)
+    [|0 .. game.stoneCoords.Length - 1|] 
+    |> Array.filter (fun i -> game.stoneStates.[i] <> Hidden) 
+    |> Array.iter (setStoneState Visible)
 
 let hideLevels num = 
-  let maxLevel = getMaxLevel game.cardCoords
+  let maxLevel = getMaxLevel game.stoneCoords
   if num >= 0 && num < maxLevel then
     game.numHiddenLevels <- num
-    game.cardCoords |> Array.iteri (fun i (_, _, h) -> 
-      setCardOpacity (if h <= maxLevel - num then 1.0 else 0.2) game.cardControls.[i])
+    game.stoneCoords |> Array.iteri (fun i (_, _, h) -> 
+      setStoneOpacity (if h <= maxLevel - num then 1.0 else 0.2) game.stoneControls.[i])
 
 let showFree () =  
   unselectAll ()
-  getFree game.cardCoords game.cardStates |> List.iter (setCardState Selected)
+  getFree game.stoneCoords game.stoneStates |> List.iter (setStoneState Selected)
 
 let showMatches () = 
   unselectAll ()
-  getMatches game.cardIDs game.cardCoords game.cardStates |> List.iter (setCardState Selected)
+  getMatches game.stoneIDs game.stoneCoords game.stoneStates |> List.iter (setStoneState Selected)
 
 let handleClick (mx, my) = 
-  let cell = match pickCell mx my with
-             | Some cellIdx when (isFree game.cardCoords game.cardStates cellIdx) -> Some cellIdx
+  let stone = match pickStone mx my with
+             | Some stoneIdx when (isFree game.stoneCoords game.stoneStates stoneIdx) -> Some stoneIdx
              | _ -> None
-  match cell, game.curSelected with
+  match stone, game.curSelected with
   | Some c, Some s when c = s -> unselectAll ()
-  | Some c, Some s when game.cardIDs.[c] = game.cardIDs.[s] -> 
+  | Some c, Some s when game.stoneIDs.[c] = game.stoneIDs.[s] -> 
       unselectAll () 
-      setCardState Hidden c 
-      setCardState Hidden s 
+      setStoneState Hidden c 
+      setStoneState Hidden s 
       game.moves <- c::s::game.moves;
-      if (game.cardStates |> Array.forall (fun st -> st = Hidden)) then 
+      if (game.stoneStates |> Array.forall (fun st -> st = Hidden)) then 
         MessageBox.Show "Amazing, you've won in this impossible game!" |> ignore
         game <- newGame 0
-      elif ((getMatches game.cardIDs game.cardCoords game.cardStates).Length = 0) then 
+      elif ((getMatches game.stoneIDs game.stoneCoords game.stoneStates).Length = 0) then 
         MessageBox.Show "No more possible moves. You've lost, sorry" |> ignore
-        shuffleVisible game.cardCoords game.cardIDs game.cardStates tryArrangeSolvable
-        updateCardVisuals()
+        shuffleVisible game.stoneCoords game.stoneIDs game.stoneStates tryArrangeSolvable
+        updateStoneControls()
   | Some c, None -> 
-      setCardState Selected c
+      setStoneState Selected c
       game.curSelected <- Some(c)
-      let url, lang = langs.[game.cardIDs.[c] - 1]
-      let status = window.FindName("CardName") :?> TextBlock
+      let url, lang = langs.[game.stoneIDs.[c] - 1]
+      let status = window.FindName("StoneName") :?> TextBlock
       status.Text <- lang
   | _, _ -> unselectAll ()
 
+let startGame id = 
+  fun _ -> game <- newGame id
+
+//============================================================================================
+//  User Input
+//============================================================================================
 let events = 
   window.MouseDown
   |> Event.filter (fun mi -> (mi.ChangedButton = Input.MouseButton.Left && 
                               mi.ButtonState = Input.MouseButtonState.Pressed))
   |> Event.map (fun mi -> (mi.GetPosition(canvas).X, mi.GetPosition(canvas).Y))
   |> Event.add handleClick
- 
-let startGame id = 
-  fun _ -> game <- newGame id
 
 let bindMenuItem (name, fn) =
   let menuItem = window.FindName(name) :?> MenuItem
@@ -421,10 +409,10 @@ let bindMenuItem (name, fn) =
 let HELP_URL = @"http://en.wikipedia.org/wiki/Mahjong_solitaire"
 
 [ "MenuUndo", undoMove
-  "MenuShuffleSolvable", fun _ -> shuffleVisible game.cardCoords game.cardIDs game.cardStates tryArrangeSolvable
-                                  updateCardVisuals()
-  "MenuShuffle", fun _ -> shuffleVisible game.cardCoords game.cardIDs game.cardStates arrangeRandom
-                          updateCardVisuals()
+  "MenuShuffleSolvable", fun _ -> shuffleVisible game.stoneCoords game.stoneIDs game.stoneStates tryArrangeSolvable
+                                  updateStoneControls()
+  "MenuShuffle", fun _ -> shuffleVisible game.stoneCoords game.stoneIDs game.stoneStates arrangeRandom
+                          updateStoneControls()
   "MenuShowFree", showFree
   "MenuShowMatches", showMatches
   "MenuHideLevel", fun _ -> hideLevels (game.numHiddenLevels + 1)
@@ -438,9 +426,9 @@ let HELP_URL = @"http://en.wikipedia.org/wiki/Mahjong_solitaire"
   "MenuAbout", fun _ -> Diagnostics.Process.Start HELP_URL |> ignore
 ] |> List.iter bindMenuItem
 
-let cardURL = window.FindName("CardURL") :?> Button
-cardURL.Click.Add(fun _ -> 
+let stoneURL = window.FindName("StoneInfoURL") :?> Button
+stoneURL.Click.Add(fun _ -> 
                     match game.curSelected with
-                    | Some sel -> Diagnostics.Process.Start (fst langs.[game.cardIDs.[sel] - 1]) |> ignore
+                    | Some sel -> Diagnostics.Process.Start (fst langs.[game.stoneIDs.[sel] - 1]) |> ignore
                     | None -> ())
 
